aad263d10eee11c9f78f3f537e6d08ab609faf0a
Luke Batts <luke.batts@research.gmit.ie>
2022-02-02 16:12:38 +0000

    update documentation of data

--- R/data.R
+++ R/data.R
@@ -1,10 +1,17 @@
 #' @name ank78
 #' @title ank78
-#' @description Catch and survey data for black-bellied anglerfish stock in the Celtic Sea and oorthern Bay of Biscay
+#' @description Catch data, survey data, survey timing and natural mortality for black-bellied anglerfish stock in the Celtic Sea and Northern Bay of Biscay
 #' @docType data
 #' @usage ank78
 #' @format a list of vectors and dataframes
-#' @author Hans Gerritsen
+#' \describe{
+#'   \item{M}{natural mortality of black bellied anglerifsh}
+#'   \item{surv_timing}{vector of survey timing for the combined IBTS fisheries survey (ibts) and the Irish monkfish and megrim survey IE-IAMS  (ieiams)}
+#'   \item{catch}{Dataframe of catch data with time series for total numbers and biomass, post-recruit numbers and biomass, and, recruit numbers and biomass.}
+#'   \item{ibts}{Dataframe of IBTS catch-per-unit-effort data with time series for total numbers and biomass, post-recruit numbers and biomass, and, recruit numbers and biomass.}
+#'   \item{ieiams}{Dataframe of IE-IAMS catch-per-unit-effort data with time series for total numbers and biomass, post-recruit numbers and biomass, and, recruit numbers and biomass.}
+#' }
+#' @author Hans Gerritsen, compiled by Luke Batts
 #' @examples
 #'
 #' data(ank78)
